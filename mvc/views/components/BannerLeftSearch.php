6fd8f58f1602e32a276145fe3f9b83e04bde44d9
trongkhang020801 <trongkhang02082001@gmail.com>
2022-06-18 15:33:05 +0700

    table paning

--- mvc/views/components/BannerLeftSearch.php
+++ mvc/views/components/BannerLeftSearch.php
@@ -8,55 +8,7 @@ $nameAirline = json_decode($data['nameAirline']);
                 <div class="pad_1">
                     <h2>Look up flight information</h2>
                     <form id="form_1" action="home/search" method="post">
-                        <div class="wrapper pad_bot1">
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                            <label class="form-check-label" for="flexRadioDefault1">
-                            Flight To
-                            </label>
-                            </div>
-                            <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                            <label class="form-check-label" for="flexRadioDefault2">
-                            Flight Away
-                            </label>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="formGroupExampleInput" class="form-label">Destination</label>
-                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter destination">
-                        </div>
-                        <div class="mb-3">
-                            <label for="formGroupExampleInput2" class="form-label">Departure</label>
-                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Enter Departure">
-                        </div>
-                        Date
-                            <div class="row">
-                                <div class='col-sm-11'>
-                                    <div class="form-group">
-                                        <div class='input-group date' id='datetimepicker1'>
-                                        <input type='text' class="form-control" />
-                                        <span class="input-group-addon">
-                                        <span class="glyphicon glyphicon-calendar"></span>
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <script type="text/javascript">
-                                    $(function () {
-                                        $('#datetimepicker1').datetimepicker();
-                                    });
-                                </script>
-                            </div>
-                            <div class="mb-3">                
-                            Choose an Airline
-                            <select class="form-select" aria-label="Default select example" name="nameair" id="nameair">
-                                <option values="0">All an Airline</option>
-                                <?php foreach($nameAirline as $item) {?>
-                                <option value=<?php echo '"'.$item->idHang.'"'?>><?php echo $item->tenHang?></option>
-                                <?php } ?>
-                                </select>
-                            </div>
+                        
                             <button type="submit" class="btn btn-primary">Search</button>
                     </form>
 
--- mvc/views/components/mainHome.php
+++ mvc/views/components/mainHome.php
@@ -4,20 +4,31 @@
 
    <article class="col2 pad_left1">
         <h2>Flight information</h2>
-        
-        <table class="table table-hover">
-            <thead>
-            <tr>
-                <th>NumID</th>
-                <th>NameFlight</th>
-                <th>Departure</th>
-                <th>Destination</th>
-                <th>TimeGo</th>
-                <th>TimeArrival</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach($infflight as $item) { ?>
+        <table id="example" class="table table-striped" width="100%">
+  <thead>
+    <tr>
+      <th class="th-sm">IdAir
+
+      </th>
+      <th class="th-sm">NameAir
+
+      </th>
+      <th class="th-sm">Departure
+
+      </th>
+      <th class="th-sm">Destination
+
+      </th>
+      <th class="th-sm">TimeGo
+
+      </th>
+      <th class="th-sm">TimeArrival
+
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($infflight as $item) { ?>
                 <tr>
                     <td><?php echo $item->soHieu?></td>
                     <td><?php echo $item->tenChuyenBay?></td>
@@ -27,7 +38,7 @@
                     <td><?php echo $item->thoigianden?></td>
                 </tr>
             <?php } ?>
-            </tbody>
-        </table>
+  </tbody>
+</table>
     </article>
 </section>
